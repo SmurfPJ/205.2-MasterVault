98473b3b0a0a0dcf6523ebc734506558a5c66acd
nicki-choo <270168718@yoobeestudent.ac.nz>
2023-10-08 15:04:02 +1300

    created base html which includes nav and logo, will work on sign up next

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def base():  # put application's code here
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
